97bdde4962568c6174ec662b2d0fc01a517632f4
KaranToor <toork@uw.edu>
2017-03-03 23:01:30 -0800

    test

--- src/SpaceBooking.java
+++ src/SpaceBooking.java
@@ -1,7 +1,9 @@
 import java.sql.Date;
 
 /**
- * Created by Ameet2r on 3/3/17.
+ * Represents an item in the SpaceBooking table.
+ *
+ * @author Karanbir Toor & Ameet Toor
  */
 public class SpaceBooking {
     private int myBookingId;
@@ -10,6 +12,15 @@ public class SpaceBooking {
     private String myLicenseNumber;
     private Date myDateOfVisit;
 
+    /**
+     * Constructs a Booking entry representing an item in the SpaceBooking table.
+     *
+     * @param theBookingId          the unique booking id.
+     * @param theParkingSpaceNumber the unique parking space number.
+     * @param theStaffNumber        the unique staff number.
+     * @param theLicenseNumber      the license number of the visitor.
+     * @param theDateOfVisit        the date the visitor is visiting. YYYY-MM-DD.
+     */
     public SpaceBooking(int theBookingId, int theParkingSpaceNumber, int theStaffNumber,
                         String theLicenseNumber, Date theDateOfVisit) {
         this.myBookingId = theBookingId;
@@ -19,42 +30,92 @@ public class SpaceBooking {
         this.myDateOfVisit = theDateOfVisit;
     }
 
+    /**
+     * Get the parking space number.
+     *
+     * @return integer representing parking space.
+     */
     public int getMyParkingSpaceNumber() {
         return myParkingSpaceNumber;
     }
 
+    /**
+     * Set the parking space number.
+     *
+     * @param myParkingSpaceNumber integer id of parking space.
+     */
     public void setMyParkingSpaceNumber(int myParkingSpaceNumber) {
         this.myParkingSpaceNumber = myParkingSpaceNumber;
     }
 
+    /**
+     * Get the unique staff number.
+     *
+     * @return integer representing staff number.
+     */
     public int getMyStaffNumber() {
         return myStaffNumber;
     }
 
+    /**
+     * Set the staff number for this reservation.
+     *
+     * @param myStaffNumber integer representing staff number.
+     */
     public void setMyStaffNumber(int myStaffNumber) {
         this.myStaffNumber = myStaffNumber;
     }
 
+    /**
+     * Gets the license number of the visitor.
+     *
+     * @return String representing license.
+     */
     public String getMyLicenseNumber() {
         return myLicenseNumber;
     }
 
+    /**
+     * Set the license number of the visitor.
+     *
+     * @param myLicenseNumber String representing license.
+     */
     public void setMyLicenseNumber(String myLicenseNumber) {
         this.myLicenseNumber = myLicenseNumber;
     }
 
+    /**
+     * Gets the date of the reservation.
+     *
+     * @return the date as YYYY-MM-DD.
+     */
     public Date getMyDateOfVisit() {
         return myDateOfVisit;
     }
 
+    /**
+     * Sets the date of the visit.
+     *
+     * @param myDateOfVisit date as YYYY-MM-DD.
+     */
     public void setMyDateOfVisit(Date myDateOfVisit) {
         this.myDateOfVisit = myDateOfVisit;
     }
 
+    /**
+     * Gets the unique booking id.
+     *
+     * @return integer booking id.
+     */
     public int getMyBookingId() {
         return myBookingId;
     }
 
+    /**
+     * Sets the booking id.
+     *
+     * @param myBookingId integer booking id.
+     */
     public void setMyBookingId(int myBookingId) {
         this.myBookingId = myBookingId;
     }
--- src/StaffMember.java
+++ src/StaffMember.java
@@ -1,7 +1,9 @@
 import java.util.List;
 
 /**
- * Created by Ameet2r on 3/3/17.
+ * Represents an item in the Staff table.
+ *
+ * @author Karanbir Toor & Ameet Toor
  */
 public class StaffMember {
 
@@ -11,6 +13,14 @@ public class StaffMember {
     private String myVehicleLicenseNumber;
     private List<SpaceBooking> myParkingSpaces;
 
+    /**
+     * Constructs a StaffMember object representing an item in the StaffMember table.
+     *
+     * @param theStaffNumber          the unique staff number.
+     * @param theName                 the name of the staff member.
+     * @param theTelephoneExtNumber   the telephone extention of the staff member.
+     * @param theVehicleLicenseNumber the vehicle license number of the staff member's car.
+     */
     public StaffMember(int theStaffNumber, String theName,
                        String theTelephoneExtNumber, String theVehicleLicenseNumber) {
         this.myStaffNumber = theStaffNumber;
@@ -19,42 +29,92 @@ public class StaffMember {
         this.myVehicleLicenseNumber = theVehicleLicenseNumber;
     }
 
+    /**
+     * Gets the unique staff number.
+     *
+     * @return the integer staff number.
+     */
     public int getMyStaffNumber() {
         return myStaffNumber;
     }
 
+    /**
+     * Sets the staff number.
+     *
+     * @param myStaffNumber the integer staff number.
+     */
     public void setMyStaffNumber(int myStaffNumber) {
         this.myStaffNumber = myStaffNumber;
     }
 
+    /**
+     * Get the name of this staff member.
+     *
+     * @return The String name.
+     */
     public String getMyName() {
         return myName;
     }
 
+    /**
+     * Sets the name of this staff member.
+     *
+     * @param myName The String name.
+     */
     public void setMyName(String myName) {
         this.myName = myName;
     }
 
+    /**
+     * Gets the telephone extention of this staff member.
+     *
+     * @return String representing telephone to preserve leading 0's.
+     */
     public String getMyTelephoneExtNumber() {
         return myTelephoneExtNumber;
     }
 
+    /**
+     * Sets the telephone number for this staff member.
+     *
+     * @param myTelephoneExtNumber String representing telephone to preserve leading 0's.
+     */
     public void setMyTelephoneExtNumber(String myTelephoneExtNumber) {
         this.myTelephoneExtNumber = myTelephoneExtNumber;
     }
 
+    /**
+     * Gets the staff member's vehicle license number.
+     *
+     * @return String license number.
+     */
     public String getMyVehicleLicenseNumber() {
         return myVehicleLicenseNumber;
     }
 
+    /**
+     * Sets the staff memmber's vehicle license number.
+     *
+     * @param myVehicleLicenseNumber String license number.
+     */
     public void setMyVehicleLicenseNumber(String myVehicleLicenseNumber) {
         this.myVehicleLicenseNumber = myVehicleLicenseNumber;
     }
 
+    /**
+     * Gets a list of booked spaces.
+     *
+     * @return The list of parking spaces booked.
+     */
     public List<SpaceBooking> getMyParkingSpaces() {
         return myParkingSpaces;
     }
 
+    /**
+     * Sets the list of spaces booked for this booking.
+     *
+     * @param myParkingSpaces The list of parking spaces booked.
+     */
     public void setMyParkingSpaces(List<SpaceBooking> myParkingSpaces) {
         this.myParkingSpaces = myParkingSpaces;
     }
